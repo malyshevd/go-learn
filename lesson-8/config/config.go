5cc22a3ebc43d19e423871a7f2e452ae8d99ee67
dmmalyshev <dmmalyshev@ozon.ru>
2021-07-06 19:12:58 +0300

    [HM-9]: config from config.json

--- lesson-8/config/config.go
+++ lesson-8/config/config.go
@@ -1,7 +1,9 @@
 package config
 
 import (
-	"flag"
+	"encoding/json"
+	"log"
+	"os"
 )
 
 type Config struct {
@@ -12,14 +14,44 @@ type Config struct {
 
 func NewConfig() *Config {
 
-	port := flag.String("port", "8080", "port number")
+	fileName := "./lesson-8/config/config.json"
+	f, err := os.Open(fileName)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer func() {
+		err = f.Close()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	fs, err := os.Stat(fileName)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	data := make([]byte, fs.Size())
+	_, err = f.Read(data)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	config := Config{}
+	err = json.Unmarshal(data, &config)
+
+	if config.Port == "" || config.Name == "" {
+		log.Fatalf("config.json incorrect: port=%v; name=%v;\n", config.Port, config.Name)
+	}
+
+	if config.Count == 0 {
+		config.Count = 1
+	}
+
+	/*port := flag.String("port", "8080", "port number")
 	name := flag.String("name", "username", "name of user")
 	count := flag.Int("count", 1, "count of repeat")
-	flag.Parse()
+	flag.Parse()*/
 
-	return &Config{
-		Port:  *port,
-		Name:  *name,
-		Count: *count,
-	}
+	return &config
 }
